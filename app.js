7321df053097055c7409322fd8054eb3b54b3d9b
Kartik <kand617@aucklanduni.ac.nz>
2020-05-16 20:24:40 +0800

    Added file logging support

--- app.js
+++ app.js
@@ -2,6 +2,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+var winston = require("winston");
+require("winston-daily-rotate-file");
+
+var transport = new winston.transports.DailyRotateFile({
+  filename: "application-%DATE%.log",
+  datePattern: "YYYY-MM-DD-HH",
+  zippedArchive: true,
+  maxSize: "20m",
+  maxFiles: "14d"
+});
+
+var logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.splat(),
+    winston.format.simple()
+  ),
+
+  transports: [transport, new winston.transports.Console()]
+});
+
 const APP_PORT = process.env.APP_PORT || 3000;
 
 const app = express();
@@ -9,9 +29,10 @@ app.use(morgan("short"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.all("", (req, res) => {
-  const data = { p: req.params, q: req.query, b: req.body };
-  console.log(JSON.stringify(data));
+app.all("*", (req, res) => {
+  const data = { params: req.params, query: req.query, body: req.body };
+
+  logger.log("info", "Recieved %s", JSON.stringify(data));
   res.send("OK");
 });
 
